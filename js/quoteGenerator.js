9b7adf04a0e476282b93c31720d0de5c5ed7fd44
amytalbott <atalbott28@gmail.com>
2018-07-27 17:11:24 -0400

    added photos, changed name

--- js/quoteGenerator.js
+++ js/quoteGenerator.js
@@ -5,17 +5,29 @@ $('#another').hide();
 // button, display a random quote
 
 var quotes = [
-  'Run until you stop',
-  'Every mile you run gets you 1.6 km closer to your goal.',
-  'The next time you feel like quitting, find a way not to somehow.',
-  "You can't stop running until you start.",
-  "You'll never know how far you can go without a measuring device of some sort.",
-  'A goal without go is just al.'
+  '<img src="./img/img0.png">',
+  '<img src="./img/img1.png">',
+  '<img src="./img/img2.png">',
+  '<img src="./img/img3.png">',
+  '<img src="./img/img4.png">',
+  '<img src="./img/img5.png">',
+  '<img src="./img/img6.png">',
+  '<img src="./img/img7.png">',
+  '<img src="./img/img8.png">',
+  '<img src="./img/img9.png">',
+  // 'Run until you stop',
+  // 'Every mile you run gets you 1.6 km closer to your goal.',
+  // 'The next time you feel like quitting, find a way not to somehow.',
+  // "You can't stop running until you start.",
+  // "You'll never know how far you can go without a measuring device of some sort.",
+  // 'A goal without go is just al.'
 ]
 
 function newQuote(){
   var randomNumber = Math.floor(Math.random() * (quotes.length));
   document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+
+
 }
 
 // Then show the 'Give me another one!' button
@@ -27,12 +39,22 @@ $("#quoteButton").click(function(){
 // display a random quote
 
 var quotes = [
-  'Run until you stop',
-  'Every mile you run gets you 1.6 km closer to your goal.',
-  'The next time you feel like quitting, find a way not to somehow.',
-  "You can't stop running until you start.",
-  "You'll never know how far you can go without a measuring device of some sort.",
-  'A goal without go is just al.'
+  '<img src="./img/img0.png">',
+  '<img src="./img/img1.png">',
+  '<img src="./img/img2.png">',
+  '<img src="./img/img3.png">',
+  '<img src="./img/img4.png">',
+  '<img src="./img/img5.png">',
+  '<img src="./img/img6.png">',
+  '<img src="./img/img7.png">',
+  '<img src="./img/img8.png">',
+  '<img src="./img/img9.png">',
+  // 'Run until you stop',
+  // 'Every mile you run gets you 1.6 km closer to your goal.',
+  // 'The next time you feel like quitting, find a way not to somehow.',
+  // "You can't stop running until you start.",
+  // "You'll never know how far you can go without a measuring device of some sort.",
+  // 'A goal without go is just al.'
 ]
 
 function anotherQuote(){
